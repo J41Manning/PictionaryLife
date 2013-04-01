0c3167cdf5cde214c67e7771aaa2ed34a2b59ab9
Matthew Wein <mwein2009@gmail.com>
2013-04-01 19:36:00 -0400

    removed warnings

--- PictionaryLogin/src/com/example/pictionarylogin/LoginActivity.java
+++ PictionaryLogin/src/com/example/pictionarylogin/LoginActivity.java
@@ -7,19 +7,19 @@ import android.view.View;
 import android.widget.TextView;
  
 public class LoginActivity extends Activity {
-    @Override
+   
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setting default screen to login.xml
+        
         setContentView(R.layout.activity_login);
  
-        TextView registerScreen = (TextView) findViewById(R.id.link_to_register);
+        TextView registerView = (TextView) findViewById(R.id.link_to_register);
  
-        // Listening to register new account link
-        registerScreen.setOnClickListener(new View.OnClickListener() {
+        registerView.setOnClickListener(new View.OnClickListener() {
  
             public void onClick(View v) {
-                // Switching to Register screen
+                // Switch to Register screen
                 Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
                 startActivity(i);
             }
